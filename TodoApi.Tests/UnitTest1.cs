59486a90db0f3f47455326ecf1185e68f3364ec8
tvermeij <tvermeij@LNL-1H860400Z7>
2026-04-09 12:01:39 +0200

    Add project files.

---
+++ TodoApi.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Application_Should_Be_Testable()
+        {
+            Assert.True(true);
+        }
+    }
+}
